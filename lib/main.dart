cccf4416ba922e1ddbef0cb07b78b4e1e0f30ac7
akademibasaran <akademibasaran@gmail.com>
2025-01-03 21:40:20 +0300

    complete social app main page

--- lib/main.dart
+++ lib/main.dart
@@ -28,8 +28,11 @@ class LoginPage extends StatelessWidget {
             SizedBox(
               height: 55.0,
             ),
-            FlutterLogo(
-              size: 70.0,
+            Container(
+              width: 120.0,
+              height: 120.0,
+              decoration: BoxDecoration(
+                  image: DecorationImage(image: AssetImage('robot-2192617_1280.png'))),
             ),
             SizedBox(
               height: 30.0,
@@ -60,6 +63,7 @@ class LoginPage extends StatelessWidget {
               width: MediaQuery.of(context).size.width - 70.0,
               height: 180.0,
               child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Padding(
                     padding: const EdgeInsets.all(12.0),
@@ -81,11 +85,49 @@ class LoginPage extends StatelessWidget {
                       ),
                     ),
                   ),
-                  Row(
-                    children: [
-                      Text("Facebook buton"),
-                      Text("Gmail buton"),
-                    ],
+                  Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Container(
+                            alignment: Alignment.center,
+                            height: 52.0,
+                            decoration: BoxDecoration(
+                                color: Colors.indigo,
+                                borderRadius: BorderRadius.circular(20.0)),
+                            child: Text(
+                              "Facebook Giriş",
+                              style: TextStyle(
+                                fontSize: 18.0,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          width: (10.0),
+                        ),
+                        Expanded(
+                          child: Container(
+                            alignment: Alignment.center,
+                            height: 52.0,
+                            decoration: BoxDecoration(
+                                color: Colors.red[600],
+                                borderRadius: BorderRadius.circular(20.0)),
+                            child: Text(
+                              "Gmail Giriş",
+                              style: TextStyle(
+                                fontSize: 18.0,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ],
               ),
